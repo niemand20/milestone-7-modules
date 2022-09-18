f8b6e6cf9699ff6bd100290a1ad4c945e7f48590
niemand20 <zahid17290@gmail.com>
2022-09-18 11:49:50 +0100

    asynchronous method

--- js-internals/async.js
+++ js-internals/async.js
@@ -1,5 +1,17 @@
+function doSomething(){
+    console.log("third");
+}
 console.log("first");
 console.log("second");
-console.log("third");
+setTimeout(()=>{
+    console.log("third")
+})
+// setTimeout(doSomething, 2000);
+// console.log("third");
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response =>{console.log("response received");
+   return response.json()})
+  .then(json => console.log(json))
 console.log("fourth");
 console.log("fifth");
